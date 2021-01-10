e74abf8af8695f2a5efd723c8a389797433323bd
Jessy <jessy.bandya5@gmail.com>
2021-01-10 15:24:53 -0800

    sth

--- core/settings.py
+++ core/settings.py
@@ -184,20 +184,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-if os.getcwd() == '/app':
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-    #Honor the 'X-forwarded-Proto' header for request.is_secure().
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    #Allow all host headers
-    ALLOWED_HOSTS = ['hellodevdennis.herokuapp.com']
-    DEBUG = True
 
-    #Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    django_heroku.settings(locals())
+django_heroku.settings(locals())
